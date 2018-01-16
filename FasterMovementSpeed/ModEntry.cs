a4f2f1588a6944523ed7cf96a9915256cc30e678
Paul Canada <paul.canada0@gmail.com>
2018-01-15 20:07:08 -0500

    Initial commit
    Added initial ModEntry and `.gitignore` file

---
+++ FasterMovementSpeed/ModEntry.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StardewModdingAPI;
+using StardewValley;
+
+namespace FasterMovementSpeed
+{
+    class ModEntry : Mod
+    {
+
+
+        public override void Entry(IModHelper helper)
+        {
+
+        }
+    }
+}
