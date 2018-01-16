981a5f381dfcb37ec683d0f5bf5224a6f438028e
Paul Canada <paul.canada0@gmail.com>
2018-01-15 20:40:00 -0500

    Fixed keypress logic
    Fixed keypress logic to be correct.
    Added speed reverts to original when flag is not toggled.

--- FasterMovementSpeed/ModEntry.cs
+++ FasterMovementSpeed/ModEntry.cs
@@ -9,30 +9,38 @@ namespace FasterMovementSpeed
     {
 
         private const int movementSpeed = 3;
+        private const int originalAddedSpeed = 0;
         private bool isActive = false;
 
-        private void GameEvents_QuarterTick(object sender, EventArgs args)
+        private void GameEvents_UpdateTick(object sender, EventArgs args)
         {
             if (isActive)
             {
-                Game1.player.addedSpeed = 3;
+                Game1.player.addedSpeed = movementSpeed;
+            } else
+            {
+                Game1.player.addedSpeed = originalAddedSpeed;
             }
         }
 
-        private void InputButtons_KeyPress(object sender, EventArgsInput key)
+        private void ControlButtons_KeyPress(object sender, EventArgsKeyPressed key)
         {
-            if (key.Button.Equals("H"))
+            if (Context.IsPlayerFree)
             {
-                isActive = !isActive;
-                Monitor.Log("Toggling extra speed to " + isActive + ".");
+                if (key.KeyPressed.ToString().Equals("H"))
+                {
+                    isActive = !isActive;
+                    Monitor.Log("Toggling extra speed to " + isActive + ".");
+                }
             }
+
         }
 
 
         public override void Entry(IModHelper helper)
         {
-            GameEvents.QuarterSecondTick += GameEvents_QuarterTick;
-            InputEvents.ButtonPressed += InputButtons_KeyPress;
+            GameEvents.UpdateTick += GameEvents_UpdateTick;
+            ControlEvents.KeyPressed += ControlButtons_KeyPress;
         }
     }
 }
