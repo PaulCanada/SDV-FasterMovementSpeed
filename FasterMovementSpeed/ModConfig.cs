b612526c7226939fbdb648903f87eeebe2c44cfd
Paul Canada <paul.canada0@gmail.com>
2018-01-15 20:54:53 -0500

    Implement initial modconfig

---
+++ FasterMovementSpeed/ModConfig.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewValley;
+
+namespace FasterMovementSpeed
+{
+    class ModConfig : Mod
+    {
+
+        public bool isActive { get; set; } = false;
+        public int speedAmount { get; set; } = 3;
+
+        public override void Entry(IModHelper helper)
+        {
+            ModConfig config = helper.ReadConfig<ModConfig>();
+
+        }
+    }
+}
